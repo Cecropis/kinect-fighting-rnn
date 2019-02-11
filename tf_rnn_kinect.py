9b35781b7448523a578d61b00061164e3b2517bd
Liadrinz <435438602@qq.com>
2019-02-12 01:02:58 +0800

    加点comment

--- tf_rnn_kinect.py
+++ tf_rnn_kinect.py
@@ -5,55 +5,59 @@ from kinect_data import KinectDataLoader
 
 kinect_data_loader = KinectDataLoader()
 
-lr = 0.0001
-training_iters = 100000
+lr = 0.0001  # 学习率
+training_iters = 100000  # epoch
 batch_size = 256
 
-n_inputs = 63
-n_steps = 200
-n_hidden_units = 128
-n_classes = 5
+n_inputs = 63  # 21个关键点的x,y,z坐标, 构成63维向量
+n_steps = 200  # 每个动作强制200帧
+n_hidden_units = 128  # 隐藏层神经元个数
+n_classes = 5  # 动作分类(右拳、左拳、右踢、左踢、防守)
 
-x = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
-y = tf.placeholder(tf.float32, [None, n_classes])
+x = tf.placeholder(tf.float32, [None, n_steps, n_inputs])  # 输入层
+y = tf.placeholder(tf.float32, [None, n_classes])  # 输出层
 
 weights = {
-    'in': tf.Variable(tf.random_normal([n_inputs, n_hidden_units])),
-    'out': tf.Variable(tf.random_normal([n_hidden_units, n_classes]))
+    'in': tf.Variable(tf.random_normal([n_inputs, n_hidden_units])),  # 输入层-隐层 权重
+    'out': tf.Variable(tf.random_normal([n_hidden_units, n_classes])) # 隐层-输出层 权重
 }
 biases = {
-    'in': tf.Variable(tf.constant(0.1, shape=[n_hidden_units,])),
-    'out': tf.Variable(tf.constant(0.1, shape=[n_classes,]))
+    'in': tf.Variable(tf.constant(0.1, shape=[n_hidden_units,])),  # 隐层 bias
+    'out': tf.Variable(tf.constant(0.1, shape=[n_classes,]))  # 输出层 bias
 }
 
-def RNN(X, weights, biases):
-    X = tf.reshape(X, [-1, n_inputs])
-    X_in = tf.matmul(X, weights['in']) + biases['in']
-    X_in = tf.reshape(X_in, [-1, n_steps, n_hidden_units])
 
+def RNN(X, weights, biases):
+    # 为了方便计算隐层输入
+    X = tf.reshape(X, [-1, n_inputs])  # 将 shape=(256批, 200帧, 63) 的动作序列转换为 shape=(256*200, 63)
+    X_in = tf.matmul(X, weights['in']) + biases['in']  # 计算出隐层输入
+    X_in = tf.reshape(X_in, [-1, n_steps, n_hidden_units])  # 将隐层输入转换为 shape=(256批, 200帧, 128隐层神经元数)
+
+    # lstm单元
     lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden_units, forget_bias=1.0, state_is_tuple=True)
     _init_state = lstm_cell.zero_state(batch_size, dtype=tf.float32)
 
-    _, states = tf.nn.dynamic_rnn(lstm_cell, X_in, initial_state=_init_state, time_major=False)
+    outputs, states = tf.nn.dynamic_rnn(lstm_cell, X_in, initial_state=_init_state, time_major=False)
 
-    results = tf.matmul(states[1], weights['out']) + biases['out']
+    results = tf.matmul(states[1], weights['out']) + biases['out']  # shape=(256批, 5种动作)
     return results
 
-pred = RNN(x, weights, biases)
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
-train_op = tf.train.AdamOptimizer(lr).minimize(cost)
+pred = RNN(x, weights, biases)  # 预测
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))  # 代价
+train_op = tf.train.AdamOptimizer(lr).minimize(cost)  # 训练方法
 
 init = tf.initialize_all_variables()
 with tf.Session() as sess:
     sess.run(init)
     step = 0
     while step * batch_size < training_iters:
-        batch_xs, batch_ys = kinect_data_loader.next_batch(batch_size)
-        batch_xs = batch_xs.reshape([batch_size, n_steps, 63])
+        batch_xs, batch_ys = kinect_data_loader.next_batch(batch_size)  # 读进来的数据 shape=(256 batch_size, 200帧, 21个点, 3 xyz)
+        batch_xs = batch_xs.reshape([batch_size, n_steps, 63])  # 把上述 (256, 200, 21, 3) 转为 (256, 200, 63)
         sess.run([train_op], feed_dict={
             x: batch_xs,
             y: batch_ys
         })
+        # 每20个epoch用一个新的batch测试精确度
         if step % 20 == 0:
             test_xs, test_ys = kinect_data_loader.next_batch(batch_size)
             test_xs = test_xs.reshape([batch_size, n_steps, 63])
